432955718c1301aace3a2205cd297cfe10c6a089
Jared Borgese <jaredborgese@gmail.com>
2025-11-25 08:29:57 -0500

    Remove unused React import in test file
    Deleted an unnecessary import of React from TextSizeContext.test.tsx to clean up the code.

--- src/contexts/__tests__/TextSizeContext.test.tsx
+++ src/contexts/__tests__/TextSizeContext.test.tsx
@@ -6,7 +6,6 @@
 
 import { describe, it, expect, beforeEach, vi } from 'vitest';
 import { render, screen, act } from '@testing-library/react';
-import React from 'react';
 import { TextSizeProvider, useTextSize } from '../TextSizeContext';
 
 // Test component that uses the context
